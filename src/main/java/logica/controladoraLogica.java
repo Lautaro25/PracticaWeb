da46b06e34e6ba04a6ed6b8b7dd87379b743a439
Lautaro <lautarovillabona.ar@gmail.com>
2024-10-09 23:53:33 -0300

    Anotaciones hechas

--- src/main/java/logica/controladoraLogica.java
+++ src/main/java/logica/controladoraLogica.java
@@ -60,5 +60,31 @@ public class controladoraLogica {
      public void eliminarCancha(int id){
         controlPersis.eliminarCancha(id);
     }
-
+     
+    //CRUD Horario     
+      public void crearHorario(horario horario){
+        controlPersis.crearHorario(horario);
+    }
+    
+    public void eliminarHorario(int id){
+        controlPersis.eliminarHorario(id);
+    }
+    
+    public void modificarHorario(horario horario){
+        controlPersis.modificarHorario(horario);    
+    }
+    
+    //CRUD Reserva
+      public void crearReserva(reserva reserva){
+        controlPersis.crearReserva(reserva);
+    }
+    
+    public void eliminarReserva(int id){
+        controlPersis.eliminarReserva(id);
+    }
+    
+    public void modificarReserva(reserva reserva){
+        controlPersis.modificarReserva(reserva);    
+    }
 }
+
